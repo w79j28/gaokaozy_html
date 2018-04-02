72de848e870276cae1f802be18597c144c74a7b6
Wang, Jingzhu <w79j28@gmail.com>
2018-04-02 21:59:09 +0800

    init files

---
+++ js/common.js
@@ -0,0 +1,15 @@
+Zepto(function($){
+  
+	// tabbar
+	$('#fill-out').on('click', function(e){ 
+		//
+		window.location.replace('3.html');
+	});
+	
+    // observe all clicks inside #content:
+	$('#online-pay').on('click', function(e){ 
+		//
+		window.location.replace('2.html');
+	});
+
+})
