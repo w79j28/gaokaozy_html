ee50e2d870b66922a42646dd6ac90f9a92851cba
Wang, Jingzhu <w79j28@163.com>
2018-04-11 16:49:05 +0800

    update

--- js/oauth.js
+++ js/oauth.js
@@ -34,16 +34,15 @@ var SERVICE_URL = "http://a0f61c09.ngrok.io";
 	else{
 	    var auth = sessionStorage.Authorization;
 	    alert('auth:' + auth);
-		if(auth){
-			
-        }
-	    else{
-		   // normal
-		   alert(location.href);
-		   $.get(SERVICE_URL + '/wechat/portal/oauth/url?url=' + location.href, function(response, status){
+		if(auth == 'undefined'){
+			alert(location.href);
+			$.get(SERVICE_URL + '/wechat/portal/oauth/url?url=' + location.href, function(response, status){
 				alert(status);
 				window.location.replace(response);
-		   },'jsonp');
+			},'jsonp');
+        }
+	   else{
+		   // normal
 	   }	
 			
 			
