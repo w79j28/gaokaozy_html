39137d182608081da6bd7e074cb21c83338e3c21
Wang, Jingzhu <w79j28@163.com>
2018-04-11 17:06:55 +0800

    add data param.

--- js/oauth.js
+++ js/oauth.js
@@ -36,7 +36,7 @@ var SERVICE_URL = "http://a0f61c09.ngrok.io";
 	    var auth = sessionStorage.Authorization;
 	    alert('auth:' + auth);
 	    alert(location.href);
-	    $.get(SERVICE_URL + '/wechat/portal/oauth/url?url=' + location.href, function(response, status){
+	    $.get(SERVICE_URL + '/wechat/portal/oauth/url', {url: location.href}, function(response, status, xhr){
 	    	alert(response);
 	    	window.location.replace(response);
 	    },'jsonp');
