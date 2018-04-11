4f826d4f2724a00bf39d9fa7b53e34f6e3d734d7
Wang, Jingzhu <w79j28@gmail.com>
2018-04-12 00:43:26 +0800

    update js/html.

--- js/oauth.js
+++ js/oauth.js
@@ -1,10 +1,30 @@
-//var ua = window.navigator.userAgent.toLowerCase(); 
-//if (ua.match(/MicroMessenger/i) == 'micromessenger') { 
-//	//
-//} else { 
-//   window.stop ? window.stop() : document.execCommand("Stop");
-//} 
-document.write('<script type="text/javascript" src="js/config.js"></script>');
+var ua = window.navigator.userAgent.toLowerCase(); 
+if (ua.match(/MicroMessenger/i) == 'micromessenger') { 
+	//
+} else { 
+  window.stop ? window.stop() : document.execCommand("Stop");
+} 
+function loadScript(url, callback){
+	callback=callback||function(){};
+	var script = document.createElement("script")
+	script.type = "text/javascript";
+	if (script.readyState){  //IE
+		script.onreadystatechange = function(){
+			if (script.readyState == "loaded" ||
+			script.readyState == "complete"){
+				script.onreadystatechange = null;
+				callback();
+			}
+		};
+	} else {  //Others: Firefox, Safari, Chrome, and Opera
+		script.onload = function(){
+		callback();
+		};
+	}
+	script.src = url;
+	document.getElementsByTagName('head')[0].appendChild(script);
+}
+
 function getQueryString() {  
     var qs = location.search.substr(1), //
     args = {}, //
@@ -24,7 +44,7 @@ function getQueryString() {
 }
 
 
-
+function oauth(){
 	var qs = getQueryString();
 	if(qs['code'] != null){
 		alert('code num :' + qs['code'] )
@@ -37,14 +57,8 @@ function getQueryString() {
 		});
 	}
 	else{
-	    var auth = sessionStorage.Authorization;
-	   
-	    
-	    
+		var auth = sessionStorage.Authorization;
 		if(auth == null){
-			alert("55auth:" + auth + ", href:" +location.href + ", surl:" + SERVICE_URL);
-			
-			
 			$.ajax({
 				  type: 'GET',
 				  url: SERVICE_URL + '/wechat/portal/oauth/url',
@@ -62,17 +76,13 @@ function getQueryString() {
 					window.location.replace(response);
 				  },
 				  error: function(xhr, type){
-					alert('Ajax error! error=55:'+JSON.stringify(xhr) + ', type' + JSON.stringify(type))
+					alert('error!:'+JSON.stringify(xhr) + ', type' + JSON.stringify(type))
 				  }
 			});
-			
-
-        }
+		}
 	   else{
 		   // normal
 	   }	
-			
-			
-   }
-    
-
+	}
+}   
+loadScript("js/config.js", oauth);
