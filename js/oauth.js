80dae7bf6dfe6b317f017b1d9c321a8ec7eae3b1
Wang, Jingzhu <w79j28@163.com>
2018-04-13 09:51:38 +0800

    import config.js

--- js/oauth.js
+++ js/oauth.js
@@ -1,23 +1,23 @@
-function loadScript(url, callback){
-	callback=callback||function(){};
-	var script = document.createElement("script")
-	script.type = "text/javascript";
-	if (script.readyState){  //IE
-		script.onreadystatechange = function(){
-			if (script.readyState == "loaded" ||
-			script.readyState == "complete"){
-				script.onreadystatechange = null;
-				callback();
-			}
-		};
-	} else {  //Others: Firefox, Safari, Chrome, and Opera
-		script.onload = function(){
-		callback();
-		};
-	}
-	script.src = url;
-	document.getElementsByTagName('head')[0].appendChild(script);
-}
+//function loadScript(url, callback){
+//	callback=callback||function(){};
+//	var script = document.createElement("script")
+//	script.type = "text/javascript";
+//	if (script.readyState){  //IE
+//		script.onreadystatechange = function(){
+//			if (script.readyState == "loaded" ||
+//			script.readyState == "complete"){
+//				script.onreadystatechange = null;
+//				callback();
+//			}
+//		};
+//	} else {  //Others: Firefox, Safari, Chrome, and Opera
+//		script.onload = function(){
+//		callback();
+//		};
+//	}
+//	script.src = url;
+//	document.getElementsByTagName('head')[0].appendChild(script);
+//}
 
 function oauth(){
 	
@@ -59,7 +59,8 @@ var ua = window.navigator.userAgent.toLowerCase();
 if (ua.match(/MicroMessenger/i) == 'micromessenger') { 
 	//style="display: none"
 	$('body').attr('style', 'display: none');
-	loadScript("js/config.js?20180412=11", oauth);
+//	loadScript("js/config.js?20180412=11", oauth);
+	oauth();
 } else { 
  
   window.stop ? window.stop() : document.execCommand("Stop");
