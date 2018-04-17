6ed2bc70604680bf9063a4211c2ee3dbb1511ae1
Wang, Jingzhu <w79j28@163.com>
2018-04-17 14:39:11 +0800

    update oauth.js.

--- src/components/common/js/oauth.js
+++ src/components/common/js/oauth.js
@@ -1,55 +1,47 @@
 import {httpGet, httpPost} from './http.js';
 
 function oauth(){
-	
-		var auth = sessionStorage.Authorization;
-		if(!auth){
-			var pathStr = location.pathname;
-			pathStr = pathStr.substring(0, pathStr.lastIndexOf("/"));
-			httpGet('/wechat/portal/oauth/url', 
-					{url: location.protocol+"//"+location.host+pathStr+"/code.html#"+location.pathname+location.search}, 
-					function(response){
-						window.location.replace(response);
-					}, 
-					function(xhr,type){
-						var getUrlNumber = sessionStorage.GetOauthUrlNumber;
-						if(getUrlNumber){
-							getUrlNumber = parseInt(getUrlNumber) + 1;
-						}
-						else{
-							getUrlNumber = 1;
-						}
-						
-						if(getUrlNumber > 3){
-						   	window.stop ? window.stop() : document.execCommand("Stop");
-						}
-						else{
-							sessionStorage.GetOauthUrlNumber = getUrlNumber;
-							window.location.reload();
-						}
-					});
-		}
-	   else{
-		   // normal
-		   $('body').removeAttr('style');  
-	   }	
-	//}
+	var auth = sessionStorage.Authorization;
+	if(!auth){
+		var pathStr = location.pathname;
+		pathStr = pathStr.substring(0, pathStr.lastIndexOf("/"));
+		httpGet('/wechat/portal/oauth/url', 
+			{url: location.protocol+"//"+location.host+pathStr+"/code.html#"+location.pathname+location.search}, 
+			function(response){
+				window.location.replace(response);
+			}, 
+			function(xhr,type){
+				var getUrlNumber = sessionStorage.GetOauthUrlNumber;
+				if(getUrlNumber){
+					getUrlNumber = parseInt(getUrlNumber) + 1;
+				}
+				else{
+					getUrlNumber = 1;
+				}
+				
+				if(getUrlNumber > 3){
+					window.stop ? window.stop() : document.execCommand("Stop");
+				}
+				else{
+					sessionStorage.GetOauthUrlNumber = getUrlNumber;
+					window.location.reload();
+				}
+			});
+	}
+   else{
+	   // normal
+	   $('body').removeAttr('style');  
+   }	
+//}
 }   
 
-
-export default function oauthCheck(){
-	var ua = window.navigator.userAgent.toLowerCase(); 
-	if (ua.match(/MicroMessenger/i) == 'micromessenger') { 
-		//style="display: none"
-		$('body').attr('style', 'display: none');
-	//	loadScript("js/config.js?20180412=11", oauth);
-		oauth();
-	} else { 
-	  console.log('Not Weixin!');
-	  window.stop ? window.stop() : document.execCommand("Stop");
-	  //$('body')
-	  //return;
-	} 
-}
+var ua = window.navigator.userAgent.toLowerCase(); 
+if (ua.match(/MicroMessenger/i) == 'micromessenger') { 
+	$('body').attr('style', 'display: none');
+	oauth();
+} else { 
+  console.log('Not Weixin!');
+  window.stop ? window.stop() : document.execCommand("Stop");
+} 
 
 
